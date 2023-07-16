bc18549189b07384a6c5266ec5a0239488eaba43
ZAVERYGO <Hockey.nick932@gmail.com>
2023-07-17 00:42:19 +0300

    2

--- src/com/ifelse/Solution2.java
+++ src/com/ifelse/Solution2.java
@@ -1,7 +1,14 @@
 package com.ifelse;
 
+import java.util.Scanner;
+
 public class Solution2 {
     public static void main(String[] args) {
-        System.out.println("jj");
+        Scanner console = new Scanner(System.in);
+        int year = console.nextInt();
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            System.out.println(year + " год - високосный");
+        else
+            System.out.println(year + " год - не високосный");
     }
 }
