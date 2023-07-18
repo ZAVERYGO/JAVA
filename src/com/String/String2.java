94e45ea046c8447f506150a6e808bf06858ffefb
ZAVERYGO <Hockey.nick932@gmail.com>
2023-07-18 20:57:35 +0300

    2

---
+++ src/com/String/String2.java
@@ -0,0 +1,9 @@
+package com.String;
+
+//2. Заменить все грустные смайлы :( в строке на весёлые :)
+public class String2 {
+    public static void main(String[] args) {
+        String str = ":(34 34345:)i:(:)hb:(";
+        System.out.println(str.replace(":(", ":)"));
+    }
+}
