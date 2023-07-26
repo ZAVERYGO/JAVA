ff87bc8473e1bb2e54adfe5c92293e6c7fedc316
ZAVERYGO <Hockey.nick932@gmail.com>
2023-07-27 00:00:05 +0300

    2

--- src/com/Class7/GeometricFigure.java
+++ src/com/Class7/GeometricFigure.java
@@ -3,12 +3,6 @@ package com.Class7;
 public abstract class GeometricFigure implements Equals {
     abstract double getArea();
     abstract double getPerimeter();
-    public boolean isTriangle(){
-        return this instanceof Triangle;
-    }
-    public boolean isRectangle(){
-        return this instanceof Rectangle;
-    }
     @Override
     public boolean equalsArea(GeometricFigure geometricFigure) {
         return this.getArea() == geometricFigure.getArea();
