52a210082a6d982a76bd962ac1f50b5e3821507c
ZAVERYGO <Hockey.nick932@gmail.com>
2023-08-10 20:03:58 +0300

    2

---
+++ src/com/Colltction/Map/Exercise2/Car.java
@@ -0,0 +1,10 @@
+package com.Colltction.Map.Exercise2;
+
+public abstract class Car {
+    private String model;
+    private Brand brand;
+    private Year year;
+    private Model model;
+    private Weight weight;
+
+}
---
+++ src/com/Colltction/Map/Exercise2/Model.java
@@ -0,0 +1,5 @@
+package com.Colltction.Map.Exercise2;
+
+public enum Model {
+    SEDAN,CABRIOLET,HATCHBACK,COUPE
+}
