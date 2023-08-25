bb718698eb5fd032c8a1e56b91c04d6a56c2eb9a
ZAVERYGO <Hockey.nick932@gmail.com>
2023-08-25 18:04:03 +0300

    1

--- src/com/FileIO/Exercise2/Main.java
+++ src/com/FileIO/Exercise2/Main.java
@@ -1,17 +1,46 @@
 package com.FileIO.Exercise2;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //2. Задан файл с текстом, найти и вывести в консоль все слова, начинающиеся с гласной буквы.
 public class Main {
     public static void main(String[] args) throws IOException {
         Path path = Path.of("src", "com", "FileIO", "Exercise1", "text.txt");
+        List<String> result = new ArrayList<>();
         List<String> list = Files.readAllLines(path);
-        list.stream()
-                .map(str -> str.)
+        for (String str : list) {
+            if (str.isEmpty()) {
+                continue;
+            }
+            String[] string = str.split(" ");
+            for (int i = 0; i < string.length; i++) {
+                if (isUpperCase(string[i].charAt(0))) {
+                    if(string[i].endsWith("?") || string[i].endsWith(",") ||
+                            string[i].endsWith(".") || string[i].endsWith("!") ||
+                            string[i].endsWith(":") || string[i].endsWith("-") ||
+                            string[i].endsWith(";")) {
+                        char[] ch = string[i].toCharArray();
+                        string[i] = new String(Arrays.copyOf(ch, ch.length - 1));
+                    }
+                    result.add(string[i]);
+                }
+            }
+        }
+        for (int i = 0; i < result.size(); i++) {
+            if(result.get(i).isEmpty())
+                result.remove(i);
+        }
+        System.out.println(result);
+    }
+
+
+    public static boolean isUpperCase(char ch) {
+        String str = Character.toString(ch);
+        return str.equals(str.toUpperCase());
     }
 }
